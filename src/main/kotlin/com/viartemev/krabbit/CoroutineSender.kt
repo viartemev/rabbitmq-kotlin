91b05f912e12e1cd86237ca46eeabba6d2017a0e
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-01-07 21:10:58 +0200

    Pasha molodec

--- src/main/kotlin/com/viartemev/krabbit/CoroutineSender.kt
+++ src/main/kotlin/com/viartemev/krabbit/CoroutineSender.kt
@@ -5,33 +5,54 @@ import com.rabbitmq.client.ConfirmListener
 import com.rabbitmq.client.MessageProperties
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 import kotlinx.coroutines.channels.Channel as KChannel
 
-
 class CoroutineSender(private val queueName: String, private val channel: Channel) {
+    private val continuations = ConcurrentHashMap<Long, Continuation<Boolean>>()
+    private val callback = object : ConfirmListener {
+        override fun handleAck(deliveryTag: Long, multiple: Boolean) {
+            handle(deliveryTag, multiple, true)
+        }
+
+        override fun handleNack(deliveryTag: Long, multiple: Boolean) {
+            handle(deliveryTag, multiple, false)
+        }
+
+        fun handle(deliveryTag: Long, multiple: Boolean, ack: Boolean) {
+            println("deliveryTag = [$deliveryTag], multiple = [$multiple], positive = [$ack]")
+            if (multiple) {
+                (1..deliveryTag)
+                        .forEach {
+                            continuations[it]?.resume(ack)
+                            continuations.remove(it)
+                        }
+
+            } else {
+                continuations[deliveryTag]?.resume(ack)
+                continuations.remove(deliveryTag)
+            }
+        }
+    }
+
+    init {
+        channel.addConfirmListener(callback)
+    }
 
     suspend fun send(message: String) = withContext(Dispatchers.IO) {
         channel.basicPublish("", queueName, null, message.toByteArray(charset("UTF-8")))
     }
 
     suspend fun sendWithAck(message: String): Boolean {
+
         val seqNo = channel.nextPublishSeqNo
         println("seqNo: $seqNo")
 
         channel.basicPublish("", queueName, MessageProperties.PERSISTENT_BASIC, message.toByteArray(charset("UTF-8")))
-        channel.addConfirmListener(object : ConfirmListener {
-            override fun handleAck(deliveryTag: Long, multiple: Boolean) {
-                println("Ack -> tag: $deliveryTag, mult: $multiple")
-            }
 
-            override fun handleNack(deliveryTag: Long, multiple: Boolean) {
-                println("Nack -> tag: $deliveryTag, mult: $multiple")
-            }
-        })
-        return suspendCoroutine { continuation ->
-            continuation.resume(true)
-        }
+        return suspendCoroutine { continuation -> continuations[seqNo] = continuation }
     }
 }
--- src/main/kotlin/com/viartemev/krabbit/Solution_4.kt
+++ src/main/kotlin/com/viartemev/krabbit/Solution_4.kt
@@ -1,11 +1,15 @@
 package com.viartemev.krabbit
 
+import awaitString
+import com.github.kittinunf.fuel.Fuel
 import com.rabbitmq.client.ConnectionFactory
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.LongAdder
 
 fun main(args: Array<String>) {
     val factory = ConnectionFactory()
+    factory.useNio()
     factory.host = "localhost"
     factory.newConnection().use { connection ->
         connection.createChannel().use { channel ->
@@ -13,18 +17,19 @@ fun main(args: Array<String>) {
             val sender = CoroutineSender(queue, channel)
             channel.confirmSelect()
             channel.queueDeclare(queue, false, false, false, null)
-
+            val counter = LongAdder()
             runBlocking {
-                repeat(4) {
+                repeat(999) {
                     launch {
-                        val message = "Hello from coroutine ${it + 1}"
+                        val message = Fuel.get("http://localhost:8081/message").awaitString() + " $it"
                         val ack = sender.sendWithAck(message)
+                        counter.increment()
                         println(" [x] Sent '$message' ack: $ack")
                     }
                 }
             }
+            println(counter.sumThenReset())
         }
     }
     println("Done")
 }
-
