f6c7d0782cfd93397f9c78e1f89b0d2061110a36
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-01-09 11:30:26 +0200

    Add several consumers

--- src/main/kotlin/com/viartemev/thewhiterabbit/consumer/Consumer.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/consumer/Consumer.kt
@@ -4,17 +4,28 @@ import com.rabbitmq.client.CancelCallback
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.DeliverCallback
 import com.rabbitmq.client.Delivery
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
+import mu.KotlinLogging
 import kotlin.coroutines.resume
 import kotlinx.coroutines.channels.Channel as KChannel
 
+private val logger = KotlinLogging.logger {}
+
 class Consumer(private val channel: Channel) {
 
+    suspend fun consumeAutoAckAsync(queue: String, output: KChannel<String>, numberOfConsumers: Int) = coroutineScope {
+        for (i in 1..numberOfConsumers) {
+            launch { consumeAutoAckAsync(queue, output) }
+        }
+    }
+
     suspend fun consumeAutoAckAsync(queue: String, output: KChannel<String>) {
-        println("Start consuming...")
+        logger.debug { "Start consuming..." }
         while (true) {
             val delivery = consume(queue)
-            println("Got message: $delivery")
+            logger.debug { "Got message: $delivery" }
             delivery?.let {
                 val body = String(it.body)
                 output.send(body)
@@ -26,12 +37,13 @@ class Consumer(private val channel: Channel) {
         return suspendCancellableCoroutine { cont ->
             channel.basicConsume(queue, true,
                     DeliverCallback { consumerTag, message ->
-                        //FIXME close continuation
                         if (!cont.isCompleted) {
+                            //TODO handle IOException
+                            channel.basicCancel(consumerTag)
                             cont.resume(message)
                         }
                     },
-                    CancelCallback { println("Cancel") })
+                    CancelCallback { logger.debug { "Cancel" } })
         }
     }
 }
