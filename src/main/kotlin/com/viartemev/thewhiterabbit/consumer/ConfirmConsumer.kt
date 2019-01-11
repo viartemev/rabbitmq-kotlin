96a8ab1f1bcaf11156979ba87634577a995b9a31
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-01-11 14:34:28 +0200

    fix coroutine leack

--- src/main/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumer.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumer.kt
@@ -6,30 +6,26 @@ import com.rabbitmq.client.DeliverCallback
 import com.rabbitmq.client.Delivery
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.sendBlocking
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import mu.KotlinLogging
-import java.util.concurrent.LinkedBlockingQueue
-import kotlin.coroutines.Continuation
-import kotlin.coroutines.resume
 import kotlinx.coroutines.channels.Channel as KChannel
 
 private val logger = KotlinLogging.logger {}
 
 class ConfirmConsumer internal constructor(private val AMQPChannel: Channel, AMQPQueue: String) {
-    //FIXME the queue size is Integer.MAX_VALUE
-    private val continuations = LinkedBlockingQueue<Continuation<Delivery>>()
+    private val continuations = KChannel<Delivery>()
 
     init {
         AMQPChannel.basicConsume(AMQPQueue, false,
-                DeliverCallback { consumerTag, message -> continuations.take().resume(message) },
+                DeliverCallback { consumerTag, message -> continuations.sendBlocking(message) },
                 CancelCallback { logger.info { "Cancelled" } }
         )
     }
 
     suspend fun consumeWithConfirm(handler: suspend (Delivery) -> Unit, handlerDispatcher: CoroutineDispatcher = Dispatchers.Default) = coroutineScope {
-        val delivery = suspendCancellableCoroutine<Delivery> { continuations.offer(it) }
+        val delivery = continuations.receive()
         withContext(handlerDispatcher) { handler(delivery) }
         AMQPChannel.basicAck(delivery.envelope.deliveryTag, false)
     }
