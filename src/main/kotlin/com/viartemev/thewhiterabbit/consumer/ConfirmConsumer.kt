af87ff3b33b3007c7c48268118b351ecbf3b8cde
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-01-15 12:09:02 +0200

    Add endless consuming

--- src/main/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumer.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumer.kt
@@ -8,6 +8,8 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.sendBlocking
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.withContext
 import mu.KotlinLogging
 import kotlinx.coroutines.channels.Channel as KChannel
@@ -16,11 +18,15 @@ private val logger = KotlinLogging.logger {}
 
 class ConfirmConsumer internal constructor(private val AMQPChannel: Channel, AMQPQueue: String) {
     private val continuations = KChannel<Delivery>()
+    private lateinit var consTag: String
 
     init {
-        AMQPChannel.basicConsume(AMQPQueue, false,
-                DeliverCallback { consumerTag, message -> continuations.sendBlocking(message) },
-                CancelCallback { logger.info { "Cancelled" } }
+        consTag = AMQPChannel.basicConsume(AMQPQueue, false,
+            DeliverCallback { consumerTag, message -> if (consumerTag == consTag) continuations.sendBlocking(message) },
+            CancelCallback {
+                logger.info { "Consumer $consTag has been cancelled" }
+                continuations.cancel()
+            }
         )
     }
 
@@ -28,5 +34,25 @@ class ConfirmConsumer internal constructor(private val AMQPChannel: Channel, AMQ
         val delivery = continuations.receive()
         withContext(handlerDispatcher) { handler(delivery) }
         AMQPChannel.basicAck(delivery.envelope.deliveryTag, false)
+    }
+
+    suspend fun consumeWithConfirm(parallelism: Int = 3, handler: suspend (Delivery) -> Unit, handlerDispatcher: CoroutineDispatcher = Dispatchers.Default) = coroutineScope {
+        val internalJobChannel = KChannel<Unit>(parallelism)
+
+        suspend fun consumeWithConfirmNew(handler: suspend (Delivery) -> Unit, handlerDispatcher: CoroutineDispatcher = Dispatchers.Default) = coroutineScope {
+            val delivery = continuations.receive()
+            logger.debug { "Coroutine got the delivery" }
+            internalJobChannel.receive()
+            logger.debug { "Free space for new coroutine" }
+            withContext(handlerDispatcher) { handler(delivery) }
+            AMQPChannel.basicAck(delivery.envelope.deliveryTag, false)
+        }
+
+        while (true) {
+            logger.debug { "Coroutine waiting for consuming" }
+            internalJobChannel.send(Unit)
+            logger.debug { "Coroutine started consuming" }
+            launch { consumeWithConfirmNew(handler, handlerDispatcher) }
+        }
     }
 }
