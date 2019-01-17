1183c6f0c8d70af0f69baab33c72b7aa600c1f10
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-01-17 14:22:23 +0200

    add more tests

--- src/test/kotlin/com/viartemev/thewhiterabbit/queue/QueueTest.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/queue/QueueTest.kt
@@ -2,6 +2,7 @@ package com.viartemev.thewhiterabbit.queue
 
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.jackson.responseObject
+import com.rabbitmq.client.Channel
 import com.rabbitmq.client.ConnectionFactory
 import com.viartemev.thewhiterabbit.utils.RabbitMQContainer
 import kotlinx.coroutines.runBlocking
@@ -17,32 +18,56 @@ import kotlin.test.assertTrue
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class QueueTest {
 
-    private val QUEUE_NAME = "test_queue"
-    lateinit var factory: ConnectionFactory
     @Container
     private val rabbitmq = RabbitMQContainer()
+    lateinit var factory: ConnectionFactory
 
     @BeforeAll
     fun setUp() {
         rabbitmq.start()
         factory = ConnectionFactory()
         factory.host = rabbitmq.containerIpAddress.toString()
-        factory.port = rabbitmq.getMappedPort(rabbitmq.connectionPort)
+        factory.port = rabbitmq.connectionPort()
     }
 
     @Test
-    fun `queue declaration test`() = runBlocking {
+    fun `declare queue test`() = runBlocking {
+        val queueName = "declare_queue_test"
         factory.newConnection().use { connection ->
             connection.createChannel().use { channel ->
-                channel.declareQueue(QueueSpecification(QUEUE_NAME))
-                val (_, _, response) = Fuel.get("http://localhost:${rabbitmq.getMappedPort(rabbitmq.managementPort)}/api/queues").authenticate("guest", "guest").responseObject<List<QueuesHttpResponse>>()
-                val queues = response.get()
-                assertNotNull(queues)
-                assertTrue { queues.isNotEmpty() }
-                assertTrue { queues.find { it.name == QUEUE_NAME } != null }
+                channel.declareQueue(QueueSpecification(queueName))
+                assertTrue { getQueues().find { it.name == queueName } != null }
             }
         }
     }
+
+    @Test
+    fun `delete queue test`() = runBlocking {
+        val queueName = "delete_queue_test"
+        factory.newConnection().use { connection ->
+            connection.createChannel().use { channel ->
+                declareQueue(queueName, channel)
+                channel.deleteQueue(DeleteQueueSpecification(queueName))
+                assertTrue { getQueues().isEmpty() }
+            }
+        }
+    }
+
+    private fun getQueues(): List<QueuesHttpResponse> {
+        val (_, _, response) = Fuel.get("http://localhost:${rabbitmq.managementPort()}/api/queues").authenticate("guest", "guest").responseObject<List<QueuesHttpResponse>>()
+        val queues = response.get()
+        assertNotNull(queues)
+        return queues
+    }
+
+    private fun declareQueue(queueName: String, channel: Channel) = runBlocking {
+        channel.declareQueue(QueueSpecification(queueName))
+        val (_, _, response) = Fuel.get("http://localhost:${rabbitmq.managementPort()}/api/queues").authenticate("guest", "guest").responseObject<List<QueuesHttpResponse>>()
+        val queues = response.get()
+        assertNotNull(queues)
+        assertTrue { queues.isNotEmpty() }
+        assertTrue { queues.find { it.name == queueName } != null }
+    }
 }
 
-data class QueuesHttpResponse(val name: String)
+data class QueuesHttpResponse(val name: String, val messages: Long)
--- src/test/kotlin/com/viartemev/thewhiterabbit/utils/RabbitMQContainer.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/utils/RabbitMQContainer.kt
@@ -3,11 +3,15 @@ package com.viartemev.thewhiterabbit.utils
 import org.testcontainers.containers.GenericContainer
 
 class RabbitMQContainer : GenericContainer<RabbitMQContainer>("rabbitmq:3-management") {
-    val managementPort = 15672
-    val connectionPort = 5672
+    private val managementPort = 15672
+    private val connectionPort = 5672
 
     init {
         withExposedPorts(managementPort, connectionPort)
     }
+
+    fun managementPort(): Int = getMappedPort(managementPort)
+
+    fun connectionPort(): Int = getMappedPort(connectionPort)
 
 }
