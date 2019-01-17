1fd2d9584df8100cca573ee106e959933675c407
Vyacheslav Artemyev <viartemev@gmail.com>
2019-01-17 12:11:24 +0200

    Add a queue declaration test

--- src/test/kotlin/com/viartemev/thewhiterabbit/queue/QueueTest.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/queue/QueueTest.kt
@@ -3,24 +3,31 @@ package com.viartemev.thewhiterabbit.queue
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.jackson.responseObject
 import com.rabbitmq.client.ConnectionFactory
+import com.viartemev.thewhiterabbit.utils.RabbitMQContainer
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
-//FIXME add testcontainers
+@Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class QueueTest {
-    private val QUEUE_NAME = "name"
-    lateinit var factory: ConnectionFactory
 
+    private val QUEUE_NAME = "test_queue"
+    lateinit var factory: ConnectionFactory
+    @Container
+    private val rabbitmq = RabbitMQContainer()
 
     @BeforeAll
     fun setUp() {
+        rabbitmq.start()
         factory = ConnectionFactory()
-        factory.host = "localhost"
+        factory.host = rabbitmq.containerIpAddress.toString()
+        factory.port = rabbitmq.getMappedPort(rabbitmq.connectionPort)
     }
 
     @Test
@@ -28,8 +35,8 @@ class QueueTest {
         factory.newConnection().use { connection ->
             connection.createChannel().use { channel ->
                 channel.declareQueue(QueueSpecification(QUEUE_NAME))
-                val (_, _, response) = Fuel.get("http://localhost:8080/api/queues").authenticate("guest", "guest").responseObject<List<QueuesHttpResponse>>()
-                val queues = response.component1()
+                val (_, _, response) = Fuel.get("http://localhost:${rabbitmq.getMappedPort(rabbitmq.managementPort)}/api/queues").authenticate("guest", "guest").responseObject<List<QueuesHttpResponse>>()
+                val queues = response.get()
                 assertNotNull(queues)
                 assertTrue { queues.isNotEmpty() }
                 assertTrue { queues.find { it.name == QUEUE_NAME } != null }
@@ -37,6 +44,5 @@ class QueueTest {
         }
     }
 }
-
 
 data class QueuesHttpResponse(val name: String)
