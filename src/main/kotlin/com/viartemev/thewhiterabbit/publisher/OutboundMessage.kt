b4119b6d9e5697a1f9d5865ee64c3fa30b0eac1b
Vyacheslav Artemyev <viartemev@gmail.com>
2019-01-23 14:25:13 +0100

    Add several messages publishing

--- src/main/kotlin/com/viartemev/thewhiterabbit/publisher/OutboundMessage.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/publisher/OutboundMessage.kt
@@ -2,10 +2,9 @@ package com.viartemev.thewhiterabbit.publisher
 
 import com.rabbitmq.client.AMQP.BasicProperties
 
-//FIXME array in equals / hashcode
 data class OutboundMessage(
-        val exchange: String,
-        val routingKey: String,
-        val properties: BasicProperties,
-        val body: ByteArray
+    val exchange: String,
+    val routingKey: String,
+    val properties: BasicProperties,
+    val msg: String
 )
