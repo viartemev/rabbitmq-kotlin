a682f874f039e354b6d24a4f40d48eecb14582c9
Vyacheslav Artemyev <viartemev@gmail.com>
2019-02-12 20:59:17 +0200

    Add example project

---
+++ the-white-rabbit-example/test/ApplicationTest.kt
@@ -0,0 +1,24 @@
+package com.viartemev
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
