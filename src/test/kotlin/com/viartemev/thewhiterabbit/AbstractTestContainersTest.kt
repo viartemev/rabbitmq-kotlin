e472c38fd55940933bb54e041a516e19ffa9331c
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-03-18 10:27:42 +0200

    Add HOP

--- src/test/kotlin/com/viartemev/thewhiterabbit/AbstractTestContainersTest.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/AbstractTestContainersTest.kt
@@ -1,6 +1,7 @@
 package com.viartemev.thewhiterabbit
 
 import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.http.client.Client
 import com.viartemev.thewhiterabbit.utils.RabbitMQContainer
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
@@ -17,11 +18,14 @@ abstract class AbstractTestContainersTest {
     }
 
     lateinit var factory: ConnectionFactory
+    lateinit var httpRabbitMQClient: Client
+    val DEFAULT_VHOST = "/"
 
     @BeforeAll
     fun setUp() {
         factory = ConnectionFactory()
         factory.host = rabbitmq.containerIpAddress.toString()
         factory.port = rabbitmq.connectionPort()
+        httpRabbitMQClient = Client("http://${rabbitmq.containerIpAddress}:${rabbitmq.managementPort()}/api/", "guest", "guest")
     }
 }
--- src/test/kotlin/com/viartemev/thewhiterabbit/exchange/ExchangeTest.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/exchange/ExchangeTest.kt
@@ -1,23 +1,21 @@
 package com.viartemev.thewhiterabbit.exchange
 
 import com.viartemev.thewhiterabbit.AbstractTestContainersTest
-import com.viartemev.thewhiterabbit.utils.getExchange
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 class ExchangeTest : AbstractTestContainersTest() {
-    val EXCHANGE_NAME = "name"
+    private val exchangeName = "exchange_name"
 
     @Test
     fun `an exchange declaration test`() {
         factory.newConnection().use { connection ->
             connection.createChannel().use { channel ->
                 runBlocking {
-                    channel.declareExchange(ExchangeSpecification(EXCHANGE_NAME))
-                    val exchange = getExchange(EXCHANGE_NAME)
-                    assertNotNull(exchange)
+                    channel.declareExchange(ExchangeSpecification(exchangeName))
+                    assertNotNull(httpRabbitMQClient.getExchange(DEFAULT_VHOST, exchangeName))
                 }
             }
         }
@@ -28,12 +26,10 @@ class ExchangeTest : AbstractTestContainersTest() {
         factory.newConnection().use { connection ->
             connection.createChannel().use { channel ->
                 runBlocking {
-                    channel.declareExchange(ExchangeSpecification(EXCHANGE_NAME))
-                    val declaredExchange = getExchange(EXCHANGE_NAME)
-                    assertNotNull(declaredExchange)
-                    channel.deleteExchange(DeleteExchangeSpecification(EXCHANGE_NAME))
-                    val exchangeAfterDeletion = getExchange(EXCHANGE_NAME)
-                    assertNull(exchangeAfterDeletion)
+                    channel.declareExchange(ExchangeSpecification(exchangeName))
+                    assertNotNull(httpRabbitMQClient.getExchange(DEFAULT_VHOST, exchangeName))
+                    channel.deleteExchange(DeleteExchangeSpecification(exchangeName))
+                    assertNull(httpRabbitMQClient.getExchange(DEFAULT_VHOST, exchangeName))
                 }
             }
         }
