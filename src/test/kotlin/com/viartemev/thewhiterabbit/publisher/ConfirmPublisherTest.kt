4b15aa0e293fc6954e5f16d6287678a08aba73d1
Vyacheslav Artemyev <vyacheslav.artemyev@zalando.fi>
2019-03-20 10:54:43 +0200

    Fix test

--- src/test/kotlin/com/viartemev/thewhiterabbit/publisher/ConfirmPublisherTest.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/publisher/ConfirmPublisherTest.kt
@@ -1,6 +1,7 @@
 package com.viartemev.thewhiterabbit.publisher
 
 import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doNothing
 import com.nhaarman.mockito_kotlin.doThrow
 import com.nhaarman.mockito_kotlin.mock
 import com.rabbitmq.client.Channel
@@ -15,44 +16,69 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
+import org.mockito.AdditionalMatchers
 import java.io.IOException
 import java.util.concurrent.atomic.AtomicInteger
 
 
 class ConfirmPublisherTest {
+    private val poisonMessage = OutboundMessage("", "", MessageProperties.PERSISTENT_BASIC, "poisonMessage")
+    private val validMessage = OutboundMessage("", "", MessageProperties.PERSISTENT_BASIC, "validMessage")
 
     @Test
-    fun test_1() {
-        val channel = mock<Channel> {
-            on { basicPublish(any(), any(), any(), any()) } doThrow IOException("Boom")
-        }
+    fun `publish several messages with one poison message`() {
+        val channel = mock<Channel>()
+        doNothing().`when`(channel).basicPublish(any(), any(), any(), AdditionalMatchers.aryEq("validMessage".toByteArray()))
+        doThrow(IOException("Boom")).`when`(channel).basicPublish(any(), any(), any(), AdditionalMatchers.aryEq("poisonMessage".toByteArray()))
+
         val counter = AtomicInteger()
         val confirmPublisher = ConfirmPublisher(channel)
 
-        val failed = OutboundMessage("", "", MessageProperties.PERSISTENT_BASIC, "fail")
         runBlocking {
             try {
                 coroutineScope {
                     val task1 = async {
                         println("Task1 has started...")
-                        delay(100)
-                        confirmPublisher.publishWithConfirm(failed)
+                        delay(50)
+                        confirmPublisher.publishWithConfirm(poisonMessage)
                         counter.getAndAdd(1)
                         println("Task1 finished")
                     }
                     val task2 = async {
                         println("Task2 has started...")
-                        delay(2000)
-                        counter.getAndAdd(1)
+                        confirmPublisher.publishWithConfirm(validMessage)
                         println("Task2 finished")
                     }
                     val task3 = async {
                         println("Task3 has started...")
-                        delay(10000)
+                        confirmPublisher.publishWithConfirm(validMessage)
                         counter.getAndAdd(1)
                         println("Task3 finished")
                     }
                     awaitAll(task1, task2, task3)
+                }
+                fail("The method didn't throw when I expected it to")
+            } catch (e: CancellationException) {
+                println("CancellationException caught: $e")
+            }
+        }
+        assertEquals(0, counter.get())
+        assertTrue(confirmPublisher.continuations.isEmpty())
+    }
+
+    @Test
+    fun `publish batch of messages with one poison message`() {
+        val channel = mock<Channel>()
+        doNothing().`when`(channel).basicPublish(any(), any(), any(), AdditionalMatchers.aryEq("validMessage".toByteArray()))
+        doThrow(IOException("Boom")).`when`(channel).basicPublish(any(), any(), any(), AdditionalMatchers.aryEq("poisonMessage".toByteArray()))
+
+        val counter = AtomicInteger()
+        val confirmPublisher = ConfirmPublisher(channel)
+
+        runBlocking {
+            try {
+                coroutineScope {
+                    confirmPublisher.publishWithConfirmAsync(this.coroutineContext, messages = listOf(validMessage, poisonMessage)).awaitAll()
                 }
                 fail("The method didn't throw when I expected it to")
             } catch (e: CancellationException) {
