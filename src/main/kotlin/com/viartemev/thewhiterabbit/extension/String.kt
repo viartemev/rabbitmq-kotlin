9db56a1e5aaa89cde6750a15ef2a96819e2b27e4
Anusit Poyen <lupangsogood@gmail.com>
2019-10-07 14:28:45 +0700

    Add Toast Ext Feature

---
+++ src/main/kotlin/com/viartemev/thewhiterabbit/extension/String.kt
@@ -0,0 +1,7 @@
+package com.viartemev.thewhiterabbit.extension
+
+import android.content.Context
+import android.widget.Toast
+
+
+fun Context.toast(message:String) = Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
