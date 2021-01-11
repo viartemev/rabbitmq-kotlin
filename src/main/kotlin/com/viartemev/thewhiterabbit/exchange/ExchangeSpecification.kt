acb3afe203822a9d25faa7fb49be32df8fd3e4eb
Vyacheslav Artemyev <viartemev@gmail.com>
2021-01-11 13:33:55 +0300

    Document exchange specification

--- src/main/kotlin/com/viartemev/thewhiterabbit/exchange/ExchangeSpecification.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/exchange/ExchangeSpecification.kt
@@ -1,10 +1,30 @@
 package com.viartemev.thewhiterabbit.exchange
 
+import com.rabbitmq.client.BuiltinExchangeType
+
 data class ExchangeSpecification(
-        val name: String,
-        val type: ExchangeType = ExchangeType.DIRECT,
-        val durable: Boolean = false,
-        val autoDelete: Boolean = false,
-        val internal: Boolean = false,
-        val arguments: Map<String, Any> = emptyMap()
+    /**
+     * The name of the exchange
+     */
+    val name: String,
+    /**
+     * The exchange type
+     */
+    val type: BuiltinExchangeType = BuiltinExchangeType.DIRECT,
+    /**
+     * True if we are declaring a durable exchange (the exchange will survive a server restart)
+     */
+    val durable: Boolean = false,
+    /**
+     * True if the server should delete the exchange when it is no longer in use
+     */
+    val autoDelete: Boolean = false,
+    /**
+     * True if the exchange is internal, i.e. can't be directly published to by a client
+     */
+    val internal: Boolean = false,
+    /**
+     * Other properties (construction arguments) for the exchange
+     */
+    val arguments: Map<String, Any> = emptyMap()
 )
