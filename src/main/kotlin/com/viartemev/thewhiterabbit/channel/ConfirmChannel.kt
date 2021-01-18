8c54d2ad539f0f2c7b08e1ea5876f9b79b73cfd3
Vyacheslav Artemyev <viartemev@gmail.com>
2021-01-18 16:00:48 +0300

    Update docs for ConfirmChannel

--- src/main/kotlin/com/viartemev/thewhiterabbit/channel/ConfirmChannel.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/channel/ConfirmChannel.kt
@@ -3,12 +3,23 @@ package com.viartemev.thewhiterabbit.channel
 import com.rabbitmq.client.Channel
 import com.viartemev.thewhiterabbit.publisher.ConfirmPublisher
 
+/**
+ * A channel with enabled publisher acknowledgements
+ */
 open class ConfirmChannel internal constructor(private val channel: Channel) : Channel by channel {
     init {
         channel.confirmSelect()
     }
 
+    /**
+     * Build confirm publisher using the channel
+     * @see com.viartemev.thewhiterabbit.publisher.ConfirmPublisher
+     */
     fun publisher() = ConfirmPublisher(this)
 }
 
+/**
+ * Publish a message in with enabled publisher acknowledgements
+ * @see com.viartemev.thewhiterabbit.publisher.ConfirmPublisher
+ */
 suspend fun ConfirmChannel.publish(block: suspend ConfirmPublisher.() -> Unit) = block(this.publisher())
