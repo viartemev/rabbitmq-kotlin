2412ec24162817d0f641e781600cfaa823045842
Artemyev Vyacheslav <vyacheslav.artemiev@B-H0AFQ05P-0347.local>
2023-11-02 16:14:24 +0300

    Add docs

--- src/main/kotlin/com/viartemev/thewhiterabbit/channel/Connection.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/channel/Connection.kt
@@ -5,11 +5,21 @@ import com.rabbitmq.client.Connection
 
 
 /**
- * Create a channel with enabled publisher acknowledgements on it.Channel by channel
- * @see com.rabbitmq.client.Channel.confirmSelect()
+ * Creates a confirm channel on the given connection.
+ *
+ * @return The confirm channel that has been created.
+ *
+ * @see com.rabbitmq.client.Channel.confirmSelect
  */
 fun Connection.createConfirmChannel(): ConfirmChannel = ConfirmChannel(this.createChannel())
 
+/**
+ * Suspends the current coroutine until a confirm channel is available or created, and then executes the specified block of code on the confirm channel.
+ *
+ * @param block The block of code to be executed on the confirm channel.
+ *
+ * @return The confirm channel on which the block of code is executed.
+ */
 suspend fun Connection.confirmChannel(block: suspend ConfirmChannel.() -> Unit): ConfirmChannel {
     var channel = Channels
         .localConfirmChannels[Thread.currentThread()]
@@ -21,6 +31,12 @@ suspend fun Connection.confirmChannel(block: suspend ConfirmChannel.() -> Unit):
         .also { block(it) }
 }
 
+/**
+ * Creates a channel object for the current connection and executes the specified block of code on it.
+ *
+ * @param block the code block to be executed on the channel.
+ * @return the channel object.
+ */
 suspend fun Connection.channel(block: suspend Channel.() -> Unit): Channel {
     var channel = Channels
         .localChannels[Thread.currentThread()]
