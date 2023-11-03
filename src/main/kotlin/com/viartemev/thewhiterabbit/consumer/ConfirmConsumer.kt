d20fddb006a5ce7fe52beb1a1197e9df5b0aa9a1
Artemyev Vyacheslav <vyacheslav.artemiev@192.168.1.3>
2023-11-03 16:53:15 +0300

    Fix consumer

--- src/main/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumer.kt
+++ src/main/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumer.kt
@@ -3,58 +3,63 @@ package com.viartemev.thewhiterabbit.consumer
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Delivery
 import com.viartemev.thewhiterabbit.exception.AcknowledgeException
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
-import kotlinx.coroutines.channels.ClosedSendChannelException
-import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
 import java.io.Closeable
 import java.io.IOException
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlinx.coroutines.channels.Channel as KChannel
 
 private val logger = KotlinLogging.logger {}
 
-class ConfirmConsumer internal constructor(private val amqpChannel: Channel, amqpQueue: String, prefetchSize: Int) : Closeable {
-    private val deliveries = KChannel<Delivery>()
+/**
+ *
+ */
+class ConfirmConsumer internal constructor(
+    private val amqpChannel: Channel, amqpQueue: String, private val prefetchSize: Int
+) : Closeable {
+    private val deliveries = KChannel<Delivery>(prefetchSize)
 
     private val consTag: String
 
     init {
         amqpChannel.basicQos(prefetchSize, false)
-        consTag = amqpChannel.basicConsume(amqpQueue, false,
-            { consumerTag, message ->
-                try {
-                    //FIXME
-                    //deliveries.sendBlocking(message)
-                } catch (e: ClosedSendChannelException) {
-                    logger.debug { "Can't receive a message. Consumer $consumerTag has been cancelled" }
-                }
-            },
-            { consumerTag ->
-                logger.debug { "Consumer $consumerTag has been cancelled for reasons other than by a call to Channel#basicCancel" }
-                deliveries.cancel()
+        consTag = amqpChannel.basicConsume(amqpQueue, false, { consumerTag, message ->
+            try {
+                logger.debug { "Trying to send a message from the consumer to the channel" }
+                deliveries.trySendBlocking(message)
+                logger.debug { "The message was successfully sent to the channel" }
+            } catch (e: Exception) {
+                logger.error(e) { "Can't send a message. Consumer $consumerTag has been cancelled" }
             }
-        )
+        }, { consumerTag ->
+            logger.info { "Consumer $consumerTag has been cancelled for reasons other than by a call to Channel#basicCancel" }
+            //FIXME do we need to cancel the channel?
+            deliveries.cancel()
+        })
     }
 
-    /**
-     * Consume a message and handle it.
-     * @throws com.viartemev.thewhiterabbit.exception.AcknowledgeException if can't send ack
-     */
     suspend fun consumeMessageWithConfirm(handler: suspend (Delivery) -> Unit) {
         try {
+            logger.debug { "Trying to receive a message from the channel" }
             val delivery = deliveries.receive()
+            logger.debug { "The message was received from the channel" }
             val deliveryTag = delivery.envelope.deliveryTag
             handler(delivery)
             try {
+                //TODO with context?
                 amqpChannel.basicAck(deliveryTag, false)
+                //TODO fix exception handling
             } catch (e: IOException) {
                 val errorMessage = "Can't ack a message with deliveryTag: $deliveryTag"
                 logger.error { errorMessage }
                 throw AcknowledgeException(errorMessage)
             }
+            //TODO exception handling
         } catch (e: Exception) {
             when (e) {
                 is ClosedReceiveChannelException -> throw CancellationException()
@@ -63,37 +68,18 @@ class ConfirmConsumer internal constructor(private val amqpChannel: Channel, amq
         }
     }
 
-    /**
-     * Consume a message and handle it with timeout.
-     * @throws kotlinx.coroutines.TimeoutCancellationException if timeout expired
-     */
-    suspend fun consumeMessageWithConfirmAndTimeout(handler: suspend (Delivery) -> Unit, timeMillis: Long) {
-        withTimeout(timeMillis) { consumeMessageWithConfirm(handler) }
-    }
-
-    /**
-     * Asynchronously consume and handle numberOfMessages.
-     */
-    suspend fun consumeMessagesWithConfirm(
-        numberOfMessages: Int,
-        handler: suspend (Delivery) -> Unit,
-        coroutineContext: CoroutineContext = EmptyCoroutineContext
-    ) = coroutineScope {
-        (1..numberOfMessages).map { async(coroutineContext) { consumeMessageWithConfirm(handler) } }
-    }
-
-    /**
-     * @todo Infinite consuming and handling messages
-     * THIS FUNCTION IS NOT PRODUCTION READY.
-     * Infinite consuming and handling messages.
-     */
-    suspend fun consumeMessagesWithConfirm1(parallelism: Int, handler: suspend (Delivery) -> Unit) = coroutineScope {
-        val channel = KChannel<Unit>(parallelism)
-        while (true) {
+    //TODO context + dispatcher
+    //TODO test cancellation & exception handling
+    suspend fun consumeMessagesWithConfirm(handler: suspend (Delivery) -> Unit) = coroutineScope {
+        val channel = KChannel<Unit>(prefetchSize)
+        while (isActive) {
             channel.send(Unit)
             launch {
-                consumeMessageWithConfirm(handler)
-                channel.receive()
+                try {
+                    consumeMessageWithConfirm(handler)
+                } finally {
+                    channel.receive()
+                }
             }
         }
     }
--- src/test/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumerTest.kt
+++ src/test/kotlin/com/viartemev/thewhiterabbit/consumer/ConfirmConsumerTest.kt
@@ -7,35 +7,79 @@ import com.viartemev.thewhiterabbit.channel.publish
 import com.viartemev.thewhiterabbit.queue.QueueSpecification
 import com.viartemev.thewhiterabbit.queue.declareQueue
 import com.viartemev.thewhiterabbit.utils.createMessage
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 class ConfirmConsumerTest : AbstractTestContainersTest() {
 
     private val QUEUE_NAME = "test_queue"
 
     @Test
-    fun `test message consuming`() {
+    fun `test message consuming`(): Unit = runBlocking {
         val counter = AtomicInteger()
         factory.newConnection().use { connection ->
-            runBlocking {
-                connection.confirmChannel {
-                    declareQueue(QueueSpecification(QUEUE_NAME))
+            connection.confirmChannel {
+                declareQueue(QueueSpecification(QUEUE_NAME))
+                val timeInMillis = measureTimeMillis {
                     publish {
-                        val messages = (1..10).map { createMessage(queue = QUEUE_NAME, body = "1") }
-                        messages.forEach { m -> publishWithConfirm(m) }
+                        (1..10).map { createMessage(queue = QUEUE_NAME, body = "1") }
+                            .map { m -> async { publishWithConfirm(m) } }.awaitAll()
                     }
-                    consume(QUEUE_NAME, 2) {
-                        for (i in 1..10) consumeMessageWithConfirm {
-                            delay(100)
-                            counter.getAndAdd(String(it.body).toInt())
-                        }
+                }
+                //TODO run millions of coroutines and try to cancel them...system continue to waste resources
+                println("(The operation took $timeInMillis ms)")
+                val timeInMillis2 = measureTimeMillis {
+                    consume(QUEUE_NAME, 100) {
+//                        for (i in 1..5000) {
+//                            consumeMessageWithConfirm {
+//                                delay(100)
+//                                counter.getAndAdd(String(it.body).toInt())
+//                            }
+//                        }
+                        (1..10).map {
+                            async(Dispatchers.IO) {
+                                consumeMessageWithConfirm {
+                                    delay(50000)
+                                    counter.getAndAdd(String(it.body).toInt())
+                                }
+                            }
+                        }.awaitAll()
                     }
                     assertEquals(10, counter.get())
                 }
+                println("(The operation2 took $timeInMillis2 ms)")
+            }
+        }
+    }
+
+    @Test
+    fun `test infinite message consuming`(): Unit = runBlocking {
+        val counter = AtomicInteger()
+        factory.newConnection().use { connection ->
+            connection.confirmChannel {
+                declareQueue(QueueSpecification(QUEUE_NAME))
+                val message = createMessage(queue = QUEUE_NAME, body = "1")
+                val sender = launch {
+                    publish {
+                        while (isActive) {
+                            delay(100)
+                            publishWithConfirm(message)
+                        }
+                    }
+                }
+                consume(QUEUE_NAME, 2) {
+                    consumeMessagesWithConfirm {
+                        println("Consuming message: ${it.body}")
+                        delay(5000)
+                        counter.getAndAdd(String(it.body).toInt())
+                    }
+                }
+                delay(50000)
+                println("Shouting down...")
+                sender.cancel()
             }
         }
     }
